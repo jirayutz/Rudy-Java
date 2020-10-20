b5f329fa48dc632f839af16705f1e158afb2c949
jirayut yodhong <amz_classic@hotmail.com>
2020-10-20 11:31:40 +0700

    Show Uid

--- app/src/main/java/com/jry/rudyjava/HomeActivity.java
+++ app/src/main/java/com/jry/rudyjava/HomeActivity.java
@@ -1,14 +1,31 @@
 package com.jry.rudyjava;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class HomeActivity extends AppCompatActivity {
+
+    TextView tvShow;
+    Button btnOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        tvShow = (TextView) findViewById(R.id.tvShow);
+        btnOK = (Button) findViewById(R.id.btnOK);
+        Intent intent = getIntent();
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Useruid = intent.getStringExtra("UserUid");
+                tvShow.setText(Useruid);
+            }
+        });
     }
 }
